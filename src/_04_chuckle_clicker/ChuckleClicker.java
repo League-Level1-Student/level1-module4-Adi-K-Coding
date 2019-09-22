b5164f490a88c511dd982f6e0852e4013be51810
League Student <league@iMac-12.attlocal.net>
2019-09-22 16:00:16 -0700

    chuckle clicker, typing tutor(IP change color of frame back to normal after resetting letter

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,22 +1,40 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
-
+public class ChuckleClicker implements ActionListener{
+	 JButton jokebutton = new JButton("Joke");
+		JButton punchlinebutton = new JButton("Punchline");
 	public static void main(String[] args) {
 		ChuckleClicker cc= new ChuckleClicker();
-		makeButton();
+		cc.makeButton();
 
 	}
-  static void makeButton() {
+   void makeButton() {
 	JFrame frame=new JFrame();
+	frame.setVisible(true);
 	JPanel panel= new JPanel();
 	frame.add(panel);
-	JButton b1 = new JButton();
-	JButton b2 = new JButton();
-}
+	panel.add(jokebutton);
+	panel.add(punchlinebutton);
+	jokebutton.addActionListener(this);
+	punchlinebutton.addActionListener(this);
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ frame.pack();
+   }
+ 
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==jokebutton) {
+			JOptionPane.showMessageDialog(null, "A blind man walks into a bar.");
+		}else if(e.getSource()==punchlinebutton){
+			JOptionPane.showMessageDialog(null, "And a table, and a chair.");
+		}
+		
+	}
 }
---
+++ src/_05_typing_tutor/TypingTutor.java
@@ -0,0 +1,56 @@
+package _05_typing_tutor;
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TypingTutor implements KeyListener {
+	char currentLetter;
+	JFrame frame = new JFrame("Typing Tutor");
+	JPanel panel= new JPanel();
+	JLabel label = new JLabel();
+	public static void main(String[] args) {
+		TypingTutor tt = new TypingTutor();
+		tt.run();
+	}
+
+	void run() {
+		currentLetter = generateRandomLetter();
+	frame.setVisible(true);	
+	label.setText(currentLetter+"");
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	frame.addKeyListener(this);
+	panel.add(label);
+	}
+
+	char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("You pressed:"+e.getKeyChar());
+		if(e.getKeyChar()==currentLetter) {
+			frame.setBackground(new Color(0,255,0));
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		currentLetter = generateRandomLetter();
+		label.setText(currentLetter+"");
+	}
+}
